9219dfd45b1aa563d2d91c566ffef3ae5d239065
Graham Kelly <gkgoat6700@gmail.com>
2024-08-19 23:20:06 -0400

    spit

--- ssa-traits/src/op.rs
+++ ssa-traits/src/op.rs
@@ -3,8 +3,9 @@ use crate::{Func, HasValues};
 pub trait OpValue<F: Func,O>{
     type Residue;
     type Capture: HasValues<F>;
-    fn disasm(self, f: &mut F) -> Result<(O,Self::Capture),Self::Residue>;
-    fn of(f: &mut F,o: O, c: Self::Capture) -> Self;
+    type Spit;
+    fn disasm(self, f: &mut F) -> Result<(O,Self::Capture,Self::Spit),Self::Residue>;
+    fn of(f: &mut F,o: O, c: Self::Capture,s: Self::Spit) -> Self;
     fn lift(f: &mut F,r: Self::Residue) -> Self;
 }
 
