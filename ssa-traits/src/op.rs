9b349068495a712d0a0fce8328d82902e149f4b7
Graham Kelly <gkgoat6700@gmail.com>
2024-08-19 23:18:09 -0400

    mut f

--- ssa-traits/src/op.rs
+++ ssa-traits/src/op.rs
@@ -3,8 +3,8 @@ use crate::{Func, HasValues};
 pub trait OpValue<F: Func,O>{
     type Residue;
     type Capture: HasValues<F>;
-    fn disasm(self) -> Result<(O,Self::Capture),Self::Residue>;
-    fn of(o: O, c: Self::Capture) -> Self;
-    fn lift(r: Self::Residue) -> Self;
+    fn disasm(self, f: &mut F) -> Result<(O,Self::Capture),Self::Residue>;
+    fn of(f: &mut F,o: O, c: Self::Capture) -> Self;
+    fn lift(f: &mut F,r: Self::Residue) -> Self;
 }
 
