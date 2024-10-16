0d5db92152271896496698c829c7f06719e1d197
Graham Kelly <gkgoat6700@gmail.com>
2024-10-16 09:35:21 -0400

    pre 0.4

--- tac-traits/src/lib.rs
+++ tac-traits/src/lib.rs
@@ -4,17 +4,33 @@ extern crate alloc;
 use alloc::boxed::Box;
 use arena_traits::Arena;
 use cfg_traits::BlockI;
+use sift_trait::Sift;
 
-pub trait Func: register_machine_traits::Func<Blocks: Arena<Self::Block,Output: Block<Self>>>{
-
+pub trait Func:
+    register_machine_traits::Func<Blocks: Arena<Self::Block, Output: Block<Self>>>
+{
 }
-impl<T: register_machine_traits::Func<Blocks: Arena<Self::Block,Output: Block<Self>>> + ?Sized> Func for T{
-
+impl<
+        T: register_machine_traits::Func<Blocks: Arena<Self::Block, Output: Block<Self>>> + ?Sized,
+    > Func for T
+{
 }
-pub trait Block<F: Func<Blocks: Arena<F::Block,Output = Self>> + ?Sized>: cfg_traits::Block<F>{
+pub trait Block<F: Func<Blocks: Arena<F::Block, Output = Self>> + ?Sized>:
+    cfg_traits::Block<F>
+{
     type Item;
-    fn values<'a>(&'a self) -> Box<dyn Iterator<Item = (F::Reg,&'a Self::Item)> + 'a>;
-    fn values_mut<'a>(&'a mut self) -> Box<dyn Iterator<Item = (&'a mut F::Reg,&'a mut Self::Item)> + 'a>;
-    fn add_value(&mut self, target: F::Reg, item: Self::Item);
+    ///Exactly ONE register per LValue (optionally a store, or similar)
+    type LValue: Sift<F::Reg, Residue: AsRef<F::Reg> + AsMut<F::Reg>>
+        + AsRef<F::Reg>
+        + AsMut<F::Reg>; 
+    fn values<'a>(&'a self) -> Box<dyn Iterator<Item = (&'a Self::LValue, &'a Self::Item)> + 'a>;
+    fn values_mut<'a>(
+        &'a mut self,
+    ) -> Box<dyn Iterator<Item = (&'a mut Self::LValue, &'a mut Self::Item)> + 'a>;
+    fn add_value(&mut self, target: Self::LValue, item: Self::Item);
 }
+
 pub type ItemI<F> = <BlockI<F> as Block<F>>::Item;
+pub type LValueI<F> = <BlockI<F> as Block<F>>::LValue;
+pub type LValueNonRegI<F> =
+    <LValueI<F> as Sift<<F as register_machine_traits::Func>::Reg>>::Residue;
